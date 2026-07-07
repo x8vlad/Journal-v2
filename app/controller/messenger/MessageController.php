1d36534c775352f9ff7dbec4522b592ecaf5cf8b
kaine <153954710+x8vlad@users.noreply.github.com>
2026-07-07 19:53:51 +0200

    message page without filters fixing

--- app/controller/messenger/MessageController.php
+++ app/controller/messenger/MessageController.php
@@ -4,7 +4,8 @@ namespace app\controller\messenger;
 class MessageController extends \core\Controller
 {
     public function index(){
-//        $option = $_GET['userOption'];
+
+        $option = $_GET['userOption'];
 //        echo $option;
 
         $users = $this->load->model("messenger/Message");
@@ -15,14 +16,26 @@ class MessageController extends \core\Controller
 //        var_dump($data_for_users);
 //        echo "<pre>";
 //        die;
-//        $data_for_user = $users->certainUser();
+
+        $data_for_user = $users->certainUser($option);
 
         $data_for_users_view = [
             'data_for_users' => $data_for_users,
         ];
-        //if all
-        $this->render("messenger/message_view", $data_for_users_view);
 
+//        $data_for_user_view = [
+//            '$data_for_user' => $data_for_user,
+//        ];
+
+
+        //if all
+        if($option == "*"){
+//            $result = all users
+        }else{
+
+        }
+        /* @var object $result */
+        $this->render("messenger/message_view", $result);
         // !all
     }
 }
