7142ebbb6795a4fff38df62f26dd74197e2f715c
kaine <153954710+x8vlad@users.noreply.github.com>
2026-08-23 21:14:27 +0200

    add files to create an announcement page

---
+++ app/controller/announcement/AnnouncementController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controller\announcement;
+
+use core\Controller;
+
+class AnnouncementController extends Controller
+{
+
+}
---
+++ app/model/announcement/Announcement.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\model\announcement;
+
+use core\Model;
+
+class Announcement extends Model
+{
+
+}
