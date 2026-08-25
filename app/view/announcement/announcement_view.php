25edc2a72df1a1ec60cbce14d8c6cc6518e6cda9
kaine <153954710+x8vlad@users.noreply.github.com>
2026-08-25 20:57:49 +0200

    Connect bootstrap CDN and add modal window to add an annuncement

--- app/view/announcement/announcement_view.php
+++ app/view/announcement/announcement_view.php
@@ -15,7 +15,7 @@
                 <th>Id</th>
                 <th>Title</th>
                 <th>Content</th>
-                <th>Data</th>
+                <th>Data and Time</th>
                 <th>Actions</th>
             </tr>
             <tbody id="table-body">
@@ -26,21 +26,57 @@
                 <td> <?= htmlspecialchars($announcement['title']) ?> </td>
                 <td> <?= htmlspecialchars($announcement['content']) ?> </td>
                 <td> <?= htmlspecialchars($announcement['created_at']) ?> </td>
+                <td><button class="btn btn-outline-dark editBtn me-2" data-id="<?=$announcement['id']?>">Edit</button>
+                <button class="btn btn-outline-danger DeleteBtn" data-id="<?=$announcement['id']?>">Delete</button></td>
             </tr>
             <?php endforeach; ?>
             </tbody>
             <!-- php endwhile -->
         </table>
 
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modalWindowAddAnnouncement">
+            Add announcement
+        </button>
 
-
-        <a href=" ?>view/add_announcement.tpl.php" class="btn btn-dark">Add announcement</a>
-
+<!--        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modalWindowAddAnnouncement">Add announcement</button>-->
+<!--        <a class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#">Add announcement</a>-->
         <!-- onclick="return false;" -->
         <a href="xxx ?>controllers/remove_announcement.php" class="btn btn-danger"  id="remover_btn">Remove all announcements</a>
         <!-- <button type="button" id="editor_btn" class="btn btn-outline-dark" data-toggle="modal" data-target="#editBtn">Edit announcements(id)</button> -->
         <!-- <a href="#" class="btn btn-outline-dark"  id="editor_btn" data-toggle="modal" data-target="#editBtn">Edit announcements(id)</a>  -->
+        <!--add announcemetn-->
+        <div class="modal fade" id="modalWindowAddAnnouncement" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalLabel">Form for announcement</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
 
+                    <form method="POST" action="">
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label class="form-label">Title</label>
+                                <input type="text" id="title" name="title" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Content</label>
+                                <textarea name="content" id="content" class="form-control" rows="3"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="submit" class="btn btn-success" id="attempBtn" name="btn_submit">Attempt</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+
+        <!--edit data-->
         <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -1,4 +1,5 @@
 <script src="/js/main.js" type="module"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     //const BASE_URL = "<?php //= BASE_URL ?>//";
 </script>
