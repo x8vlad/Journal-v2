5424c7ead89cdc16ba7b2272d37cf09ff98ad73f
kaine <153954710+x8vlad@users.noreply.github.com>
2026-09-03 10:38:26 +0200

    Implement Login controller, auth forms are working

--- app/controller/account/Login.php
+++ app/controller/account/Login.php
@@ -17,5 +17,24 @@ class Login extends \core\Controller
 //        return "<br>Login: {$login}<br> password: {$password}";
 
 
+
+        if(!empty($login) && !empty($password)){
+            $user = new User($this->registry);
+
+            $LoginValidatorObj = new LoginValidator($login, $password);
+            $result = $LoginValidatorObj->logInUser($user);
+
+            switch ($result) {
+                case "login success":
+                    $this->helper->renderAlert("success", "Login as: {$login}");
+                    break;
+                case "empty fields":
+                    $this->helper->renderAlert("warning", "Empty fields");
+                    break;
+                case "invalid login":
+                    $this->helper->renderAlert("danger", "invalid login");
+                    break;
+            }
+        }
     }
 }
